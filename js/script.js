f7489b43db8580aac72ec17c105b56d5af178004
FabrizioMisseri <misserifabrizio@gmail.com>
2022-10-26 16:44:13 +0200

    add dynamic title part and text part

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 // Descrizione:
 // Partendo dal markup della versione svolta in js plain, rifare lo slider ma questa volta usando Vue.
+// FATTO
 // Bonus:
 // 1- al click su una thumb, visualizzare in grande l'immagine corrispondente
 // 2- applicare l'autoplay allo slider: ogni 3 secondi, cambia immagine automaticamente
